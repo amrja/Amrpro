705dfe121c3a308d64c07aed218a8efc205a105a
amrja <38591975+amrja@users.noreply.github.com>
2019-01-30 13:38:36 +0200

    Update jqBootstrapValidation.js

--- AmrReadeen/js/jqBootstrapValidation.js
+++ AmrReadeen/js/jqBootstrapValidation.js
@@ -1,11 +1,5 @@
-/* jqBootstrapValidation
- * A plugin for automating validation on Twitter Bootstrap formatted forms.
- *
- * v1.3.6
- *
- * License: MIT <http://opensource.org/licenses/mit-license.php> - see LICENSE file
- *
- * http://ReactiveRaven.github.com/jqBootstrapValidation/
+/* 
+ Amr jamal
  */
 
 (function( $ ){
